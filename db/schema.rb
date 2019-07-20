2e72f43d41780a66559af5a0eb2936a64b2382db
BradyBromley <bcbromle@sfu.ca>
2019-07-19 19:41:51 -0700

    Changing schema back to what it originally was (I don't know why it changed)

--- db/schema.rb
+++ db/schema.rb
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2019_07_16_011846) do
 
   create_table "todo_lists", force: :cascade do |t|
     t.string "title"
+    t.string "string"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
